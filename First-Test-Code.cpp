aeea6646decd30c08b9f364cf213808993e32821
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-10-02 07:18:03 +0530

    First test file

---
+++ First-Test-Code.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+	public:
+
+		void fun(){
+			cout<<"Hello World"<<endl;
+		}
+};
+
+
+int main(){
+
+	Solution a;
+	a.fun();
+	return 0;
+}
