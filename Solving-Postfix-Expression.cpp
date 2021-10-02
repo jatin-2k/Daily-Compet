eb5eefe40118b3a5911a786b5630be7f4c3be176
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-10-02 07:18:45 +0530

    Solving postfix expression

---
+++ Solving-Postfix-Expression.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+    public:
+    //Function to evaluate a postfix expression.
+    int evaluatePostfix(string S)
+    {
+        stack<int> st;
+        
+        //iterating over the given string.
+        for (int i = 0; i<S.size(); ++i)
+        {
+            //if current character is an operand, we push it to the stack.
+            if (isdigit(S[i]))
+                st.push(S[i] - '0');
+    
+            //else current character is an operator.
+            else
+            {
+                //we pop and store the values of first two elements of stack.
+                int val1 = st.top(); st.pop();
+                int val2 = st.top(); st.pop();
+                
+                //we perform required operation and push the result in stack.
+                switch (S[i])
+                {
+                 case '+': st.push(val2 + val1); break;
+                 case '-': st.push(val2 - val1); break;
+                 case '*': st.push(val2 * val1); break;
+                 case '/': st.push(val2 / val1); break;
+                }
+            }
+        }
+        //returning the top element of the stack.
+        return st.top();
+    }
+};
