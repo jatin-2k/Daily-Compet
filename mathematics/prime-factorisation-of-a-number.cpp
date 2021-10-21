fe8cea6cde746ddcef6ebbd3270935115a12c781
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-10-21 15:06:46 +0530

    Prime Factorisation of a nuber

---
+++ mathematics/prime-factorisation-of-a-number.cpp
@@ -0,0 +1,16 @@
+vector<int> primeFactor(int n){
+    vector<int> ans;
+    while(n%2==0){
+        ans.push_back(2);
+        n/=2;
+    }
+    for(int i=3; i<sqrt(n); i=i+2){
+        while(n%i==0){
+            ans.push_back(i);
+            n/=i;
+        }
+    }
+    if(n>1) 
+    ans.push_back(n);
+    return ans;
+}
