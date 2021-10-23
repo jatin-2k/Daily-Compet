25b813b18baae57ac20a46e99edd44f12764076a
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-10-23 17:27:23 +0530

    Check if the number is k jagged

---
+++ mathematics/check-if-k-jagged.cpp
@@ -0,0 +1,16 @@
+int spf(int num){
+    while(num%2 == 0) {
+        return 2;
+    }
+    for(int i=3; i<=sqrt(num); i=i+2){
+        while(num%i==0){
+            return i;
+            num/=i;
+        }
+    }
+    return num;
+}
+//function to check k jagged
+bool kj(int num, int k){
+    return spf(num) >= k; 
+}
