1ba0b4b1513fdf970e5edb7708f4ba9552a45075
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-10-27 11:15:28 +0530

    Insert a node in Bst

---
+++ Binary-Search-Trees/insert-a-node-in-BST.cpp
@@ -0,0 +1,12 @@
+Node* insert(Node* root, int k) {
+    // Your code here
+    if(root){
+        if(root->data == k) return root;
+        if(root->data > k) root->left = insert(root->left,k);
+        if(root->data < k) root->right = insert(root->right,k);
+        return root;
+    }
+    else{
+        return new Node(k);
+    }
+}
