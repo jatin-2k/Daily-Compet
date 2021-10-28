e6770d0223301ec7a0f26d846d345c368e1288b1
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-10-28 11:14:47 +0530

    Delete a node in BST

---
+++ Binary-Search-Trees/delete-a-node-in-bst.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+  
+    
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        
+        if(root)
+        {
+            if(root->val==key)
+            {
+                if(root->left==NULL)return root->right;
+                if(root->right==NULL)return root->left;
+                if(root->left and root->right)
+                {
+                  TreeNode* temp=root->left;  
+                  while(temp->right!=NULL)temp=temp->right;
+                    root->val=temp->val;
+                    root->left=deleteNode(root->left,root->val);
+                }
+                
+            }
+            else
+            {
+                if(root->val>key)root->left =deleteNode(root->left,key);
+                if(root->val<key)root->right=deleteNode(root->right,key);
+                
+            }
+            // return root;
+        }
+        else
+        {
+            return NULL;
+        }
+    return root;
+    }
+};
