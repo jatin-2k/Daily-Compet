e9f0af08b77d03d9859e38a8ae8cc8940563af3e
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-11-02 15:08:55 +0530

    K largest elements

---
+++ heap/k-largest-elements.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+    //Function to return k largest elements from an array.
+    vector<int> kLargest(int arr[], int n, int k)
+    {
+        // code here
+        vector<int> ans;
+        //min heap
+        priority_queue<int,vector<int>,greater<int>> q;
+        for(int i=0; i<n; i++){
+            q.push(arr[i]);
+            if(q.size()>k) q.pop();
+        }
+        while(!q.empty()){
+            ans.push_back(q.top());
+            q.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
