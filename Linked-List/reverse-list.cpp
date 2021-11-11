f3d3f937d2c5dc845273aa518498bb6ea4785990
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-11-11 21:34:37 +0530

    Reverse a linked list

---
+++ Linked-List/reverse-list.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {
+        if(head==NULL)
+            return NULL;
+        
+        //taking three pointers to store the current, previous and next nodes.
+        struct Node *prev = NULL;   
+        struct Node *current = head;
+        struct Node *next= current->next;
+        
+        while(current!=NULL)
+        {
+            //taking the next node as next.
+            next=current->next;
+            
+            //storing the previous node in link part of current node.
+            current->next=prev; 
+            
+            //updating prev from previous node to current node.
+            prev=current; 
+            
+            //updating current node to next node.
+            current =next;        
+        }
+        
+        return prev;
+    }
+};
