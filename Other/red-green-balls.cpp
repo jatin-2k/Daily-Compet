0a950a1ae1b85df15963027caf535d541b074881
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-12-28 01:56:26 +0530

    Red Green Ball -input correction

--- Other/red-green-balls.cpp
+++ Other/red-green-balls.cpp
@@ -1,58 +1,69 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
 bool isPossible(vector<int> nums, int n){
-    sort(nums.begin(), nums.end());
-    for(int i=0; i<n; i++) if(nums[i] > i+1) return false;
-    return true;
+	sort(nums.begin(), nums.end());
+	for(int i=0; i<n; i++) if(nums[i] > i+1) return false;
+	return true;
 }
+
 int moveZeroDown(int x, vector<int>& nums, int n){
-    int ans = 0;
-    if(nums[x+1]==0) ans += moveZeroDown(x+1, nums, n);
-    ans++; nums[x]++; nums[x+1]--;
-    swap(nums[x],nums[x+1]);
-    return ans;
+	int ans = 0;
+	if(nums[x+1]==0) ans += moveZeroDown(x+1, nums, n);
+	ans++; nums[x]++; nums[x+1]--;
+	swap(nums[x],nums[x+1]);
+	return ans;
 }
+
 int solve(vector<vector<bool>> &arr,vector<int> nums, int n){
-    for(int i=0; i<n; i++){
-        nums[i] = (!nums[i])? i: i-nums[i]+1;
-    }
-    int ans = 0;
-    for(int i=n-1; ~i; i--){
-        if(nums[i]<0){
-            if(nums[i+1]==0){
-                ans += moveZeroDown(i+1, nums, n);
-            }
-            ans++; nums[i]++; nums[i+1]--;
-            swap(nums[i],nums[i+1]);
-            i = n-1;
-        }
-    }
-    return ans;
+	for(int i=0; i<n; i++){
+		nums[i] = (!nums[i])? i: i-nums[i]+1;
+	}
+	int ans = 0;
+	for(int i=n-1; ~i; i--){
+		if(nums[i]<0){
+			if(nums[i+1]==0){
+				ans += moveZeroDown(i+1, nums, n);
+			}
+			ans++; nums[i]++; nums[i+1]--;
+			swap(nums[i],nums[i+1]);
+			i = n-1;
+		}
+	}
+	return ans;
+}
+
+int RedGreenBalls(vector<vector<char>> &balls, int n){
+    vector<vector<bool>> arr;
+	vector<int> nums;
+	for(int i=0; i<n; i++){
+		vector<bool> temp(n); int m=0;
+		for(int j=0; j<n; j++){
+			temp[j]=(balls[i][j]=='R')?1:0;
+			if(temp[j]) m = j+1;
+		}
+		nums.push_back(m);
+		arr.push_back(temp);
+	}
+	if(isPossible(nums,n)){
+		return solve(arr,nums,n);
+	}
+	else{
+		return -1;
+	}
 }
 
 int main() {
-    vector<vector<bool>> arr;
-    int n;
-    cin>>n;
-    vector<int> nums;
-    for(int i=0; i<n; i++){
-        vector<bool> temp(n); int m=0;
-        for(int j=0; j<n; j++){
-            bool a;
-            cin>>a;
-            temp[j]=a;
-            if(temp[j]) m = j+1;
-        }
-        nums.push_back(m);
-        arr.push_back(temp);
-    }
-    if(isPossible(nums,n)){
-        cout<<solve(arr,nums,n);
-    }
-    else{
-        cout<<-1;
-    }
-    return 0;
+	vector<vector<char>> arr;
+	int n;
+	cin>>n;
+	for(int i=0; i<n; i++){
+		vector<char> temp(n);
+		for(int j=0; j<n; j++){
+            cin>>temp[j];
+		}
+		arr.push_back(temp);
+	}
+    cout<<RedGreenBalls(arr,n);
+	return 0;
 }
- 
