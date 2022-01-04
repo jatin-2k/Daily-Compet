df61f6d9dfa899bdcd48814416f8c777245dfd80
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-01-04 19:11:34 +0530

    Exactly 3 divisors

---
+++ mathematics/exactly-3-divisors.cpp
@@ -0,0 +1,31 @@
+class Solution{
+    public:
+    int exactly3Divisors(int n)
+    {
+        //Your code here
+        int count=0;
+        for(int i = 2; i * i <= n; i++)
+        {
+            if (isPrime(i))
+            {
+                if (i * i <= n)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    bool isPrime(int n)
+    {
+        if (n == 0 || n == 1)
+            return false;
+             
+        for(int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+};
