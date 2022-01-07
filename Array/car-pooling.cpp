db4f12adaa272f3553874e14c5ce79059018ded6
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-01-07 22:00:37 +0530

    Car pooling refactored

--- Array/car-pooling.cpp
+++ Array/car-pooling.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
-    static bool cmp(vector<int> &a, vector<int> &b){
-        return a[1]<b[1];
-    }
     vector<int> d = vector<int>(1001, 0);
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        sort(trips.begin(), trips.end(), cmp);
         for(auto i: trips){
             d[i[1]] += i[0];
             d[i[2]] -= i[0];
