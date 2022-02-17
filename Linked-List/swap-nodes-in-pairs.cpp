f528b8e9b59270f936795058561240ba3670bc3d
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-02-17 17:50:59 +0530

    Swap nodes in pairs

--- Linked-List/swap-nodes-in-pairs.cpp
+++ Linked-List/swap-nodes-in-pairs.cpp
@@ -21,3 +21,14 @@ public:
         return dummy->next;
     }
 };
+
+ListNode* swapPairs(ListNode* head) {
+    ListNode **pp = &head, *a, *b;
+    while ((a = *pp) && (b = a->next)) {
+        a->next = b->next;
+        b->next = a;
+        *pp = b;
+        pp = &(a->next);
+    }
+    return head;
+}
