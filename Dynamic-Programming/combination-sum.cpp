8e6b8b5ee33731c81d13157f73510e912a19d802
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-02-17 10:41:16 +0530

    Combination sum v2

--- Dynamic-Programming/combination-sum.cpp
+++ Dynamic-Programming/combination-sum.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        set<vector<int>> dp[target+1];
-        dp[0]={{}};
-        for(int i=0; i<=target; i++){
-            for(auto cur: candidates){
-                if(i>=cur){
-                    for(vector<int> v: dp[i-cur]){
-                        v.push_back(cur);
-                        sort(v.begin(), v.end());
-                        dp[i].insert(v);
-                    }
-                }
-            }
-        }
-        vector<vector<int>> ans;
-        for(auto v: dp[target]){
-            vector<int> tmp;
-            for(auto i: v){
-                tmp.push_back(i);
-            }
-            ans.push_back(tmp);
-        }
+        vector<int> temp;
+        combinationSum(candidates, target, temp, 0);
         return ans;
+    }
+    void combinationSum(vector<int>& candidates, int target, vector<int> &cur, int id) {
+        if(target == 0){ ans.push_back(cur); return; }
+        if(target < 0 || id >= candidates.size()) return;
+        combinationSum(candidates, target, cur, id+1);
+        cur.push_back(candidates[id]);
+        combinationSum(candidates, target - candidates[id], cur, id);
+        cur.pop_back();
     }
 };
