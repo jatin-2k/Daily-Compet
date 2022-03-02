73916043770083c33aae9703a6b8c1a1ef384198
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-03-02 16:42:52 +0530

    Merge 2 sorted lists

--- Linked-List/merge-2-sorted-list.cpp
+++ Linked-List/merge-2-sorted-list.cpp
@@ -1,25 +1,19 @@
-Node* sortedMerge(Node* head1, Node* head2)  
-{  
-    // code here
-    
-    Node *tail, *head;
-    tail = new Node(NULL);
-    head = tail;
-    while(head1 && head2){
-        if(head1 -> data < head2 -> data){
-            tail -> next = head1;  //joda
-            tail = head1;          //ans wali k last ele p aee
-            head1 = head1 -> next; //bachi hui k head p aae
+ListNode* mergeSorted(ListNode *l1, ListNode *l2){
+    ListNode *dummy = new ListNode(-1);
+    ListNode *cur = dummy;
+    while(l1 and l2){
+        if(l1->val < l2->val){
+            cur->next = l1;
+            l1 = l1->next;
         }
         else{
-            tail-> next = head2;
-            tail = head2;
-            head2 = head2 -> next;
+            cur->next = l2;
+            l2 = l2->next;
         }
-        tail -> next = NULL; //tod dia
+        cur = cur->next; 
+        cur -> next = NULL;
     }
-    if(head1) tail -> next = head1;
-    if(head2) tail -> next = head2;
-    
-    return head->next;
-}  
+    if(l1) cur -> next = l1;
+    if(l2) cur -> next = l2;
+    return dummy->next;
+}
