b08360760833c0eecf7a48e553ec9ed96ec24d17
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-03-19 14:41:36 +0530

    Score of parentheses

---
+++ Stacks/score-of-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        vector<int> v(50);
+        stack<int> st;
+        st.push(0);
+        for(auto c: s){
+            if(c=='('){
+                st.push(0);
+            }
+            else{
+                int v = st.top(); st.pop();
+                int w = st.top(); st.pop();
+                st.push(w + max(2*v, 1));
+            }
+        }
+        return st.top();
+    }
+};
