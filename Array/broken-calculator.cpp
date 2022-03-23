b05a4739f5c7f3878396b80f63720a929748eb24
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-03-23 08:05:00 +0530

    Broken Calculator

---
+++ Array/broken-calculator.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int brokenCalc(int start, int target) {
+        int ans =0 ;
+        while(target > start){
+            if(target%2==0)
+                target/=2;
+            else
+                target+=1;
+            ans++;
+        }
+        ans+=start-target;
+        return ans;
+    }
+};
