0b3b910ca55898fbe2669fc113555af629418ea2
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-03-23 10:03:09 +0530

    Select 2nd highest salary v2

--- Sql/select-2nd-highest-salary.sql
+++ Sql/select-2nd-highest-salary.sql
@@ -2,3 +2,11 @@ SELECT (SELECT DISTINCT SALARY
 FROM EMPLOYEE
 ORDER BY SALARY DESC
 LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
