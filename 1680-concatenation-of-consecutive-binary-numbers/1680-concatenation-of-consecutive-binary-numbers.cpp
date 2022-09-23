a406ef01a34f19e05152c15aa63eae0205c2c12c
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-09-23 17:41:09 +0530

    Time: 82 ms (69.56%), Space: 6 MB (47.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-    int concatenatedBinary(int n) {
-        string s = "";
-        long long ans = 0, m = 1000000007;
-        for(int i=1; i<=n; i++){
-            string a = "";
-            for(int j=i; j; j /= 2){
-                a += (j & 1 == 1) ? '1' : '0';
-            }
-            reverse(a.begin(), a.end());
-            s += a;
-        }
-        //cout<<s;
-        for(int i=0; i<s.size(); i++){
-            ans = (ans * 2 + (s[i] == '1'))%m;
+    long long concatenatedBinary(int n, int l = 0) {
+        long ans = 0, mod = 1e9+7;
+        for (int i = 1; i <= n; ++i) {
+            int len = 0;
+            for (int j = i; j; j >>= 1, ++len);
+            ans = ((ans << len) % mod + i) % mod;
         }
         return ans;
     }
