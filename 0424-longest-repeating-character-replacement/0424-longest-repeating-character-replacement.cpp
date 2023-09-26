c84b30062a346bde20bd11350c25fb865868fffb
Jatin Agrawal <jatinagrawal2000@gmail.com>
2023-09-26 23:16:53 +0530

    Time: 8 ms (63.24%), Space: 7.3 MB (22.75%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -5,23 +5,14 @@ public:
         int n = s.size();
         int ans = 1;
         int maxChar = 1;
-        char c = s[0];
         for(int r=0, l=0; r<n; r++){
             mp[s[r]-'A']++;
             if(mp[s[r]-'A'] > maxChar){
                 maxChar = mp[s[r]-'A'];
-                c = s[r];
             }
             while(l<r and r-l+1-maxChar > k){
                 mp[s[l]-'A']--;
-                maxChar--;
                 l++;
-                for(int i=0; i<26; i++){
-                    if(mp[i] > maxChar){
-                        maxChar = mp[i];
-                        c = i+'A';
-                    }
-                }
             }
             //if(ans < r-l+1) cout<<l<<","<<r<<","<<maxChar<<","<<c<<": "<<s.substr(l,r-l+1)<<"\n";
             ans = max(ans, r-l+1);
