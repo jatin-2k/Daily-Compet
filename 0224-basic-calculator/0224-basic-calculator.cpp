edcf922f598771d7d3d6154b21eda9a5b61da3d1
Jatin Agrawal <jatinagrawal2000@gmail.com>
2024-03-09 15:06:24 +0530

    Time: 4 ms (87.81%), Space: 10.1 MB (30.32%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
-    int calculate(string s,int start = 0) {
-       function<int(int&)> helper = [&](int& i)-> int{
-           long res = 0,num = 0,sign = 1;
-           while(i<s.size() and s[i] != ')'){
-               if(isdigit(s[i]))num  = num*10 + s[i] - '0';
-                else{
-                     res+= num*sign, num = 0;
-                     if(s[i] == '(')res = res + sign*helper(++i);
-                     else if(!isspace(s[i])) sign = s[i] == '+'?1:-1;
-                   }
-                  ++i;
-                }
-            return res + sign*num;
-        };
-        return helper(start);
+    int calculate(string s) {
+        long long int ans = 0, num=0, sign=1;
+        int i = 0;
+        stack<int> st;
+
+        for(i = 0 ; i < s.size() ;i++){
+            if(isdigit(s[i])){
+                num = num*10 + s[i]-'0';
+            }else if(s[i] == '+' || s[i] == '-'){
+                ans = ans + sign*num;
+                num = 0;
+                sign = s[i] == '+' ? 1: -1;
+            }else if(s[i] == '('){
+                st.push(ans);
+                st.push(sign);
+                sign = 1;
+                ans = 0;
+            }else if(s[i] == ')'){
+                
+                int tmp_sign = st.top();
+                st.pop();
+                int tmp_ans =st.top();
+                st.pop();
+                ans = ans + sign*num;
+                ans = tmp_ans + tmp_sign * ans;
+
+                sign = 1;
+                num=0;
+
+            }
+        }
+        ans = ans + sign*num;
+        
+        return ans;
     }
 };
