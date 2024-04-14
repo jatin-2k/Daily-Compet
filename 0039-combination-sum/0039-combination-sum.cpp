a65e9e50906bd8e2b61993f7dcc4b37321490b12
Jatin Agrawal <jatinagrawal2000@gmail.com>
2024-04-15 00:46:41 +0530

    Time: 4 ms (73.96%), Space: 16.4 MB (33.60%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -4,21 +4,16 @@ public:
     vector<int> cur;
     vector<vector<int>> combinationSum(vector<int>& arr, int t) {
         int n = arr.size();
-        foo(arr, t, 0);
+        combinationSum(arr, t, 0);
         return ans;
     }
     
-    void foo(vector<int>& arr, int t, int idx){
-        if(0 > t) return;
-        if(0 == t){
-            ans.push_back(cur);
-            return;
-        }
-        for(int i=idx; i<arr.size(); i++){
-            int x = arr[i];
-            cur.push_back(x);
-            foo(arr, t - x, i);
-            cur.pop_back();
-        }
+    void combinationSum(vector<int>& c, int t, int idx){
+        if(t==0){ ans.push_back(cur); return; }
+        if(t < 0 || idx >= c.size()){ return; }
+        combinationSum(c, t, idx+1);
+        cur.push_back(c[idx]);
+        combinationSum(c, t-c[idx], idx);
+        cur.pop_back();
     }
 };
